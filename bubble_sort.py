a0ceec9b44af164527f9689d8d464b95b7009e2c
Neerja Narayanappa <n.neerja28@gmail.com>
2019-02-03 10:24:31 -0600

    Create bubble_sort.py

---
+++ bubble_sort.py
@@ -0,0 +1,48 @@
+# a = [6,8,1,3,0,5]
+# [6,8,1,3,0,5] 6>8
+# [6,1,8,3,0,5] 8>1
+# [6,1,3,8,0,5] 8>3
+# [6,1,3,8,0,5] 8>0
+# [6,1,3,0,5,8] 8>5
+
+# [1,6,3,0,5,8] 6>1
+# [1,3,6,0,5,8] 6>3
+# [1,3,0,6,5,8] 6>0
+# [1,3,0,5,6,8] 6>5
+# [1,3,0,5,6,8] 6>8
+
+# [1,3,0,5,6,8] 1>3
+# [1,0,3,5,6,8] 3>0
+# [1,0,3,5,6,8] 3>5
+# [1,0,3,5,6,8] 5>6
+# [1,0,3,5,6,8] 6>8
+
+# [0,1,3,5,6,8] 1>0
+# [0,1,3,5,6,8] 1>3
+# [0,1,3,5,6,8] 3>5
+# [0,1,3,5,6,8] 5>6
+# [0,1,3,5,6,8] 6>8
+
+# the worst case list would be a list in descending order. 
+# Swapping
+# temp = a
+# a  = b
+# b = temp
+
+# Algorithm
+
+# For the first iteration, compare all the elements (n). For the subsequent runs, compare (n-1) (n-2) and so on.
+# Compare each element with its right side neighbor.
+# Swap the smallest element to the left.
+# Keep repeating steps 1-3 until the whole list is covered.
+
+def bubble_sort(a):
+  for i in range(len(a)-1,0,-1):
+    for j in range(i):
+      if a[j] > a[j+1]:
+        temp = a[j]
+        a[j]=a[j+1]
+        a[j+1]=temp
+   return a
+   
+bubble_sort([6,8,1,3,0,5])
